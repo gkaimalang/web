bf51442386ea9284fa3e878ad7b93a11c2fb9692
goenpiloes <goenpiloes@gmail.com>
2022-12-08 00:46:34 +0700

    add default page

--- gkaimalang/gkaimalang/urls.py
+++ gkaimalang/gkaimalang/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
+
+def defaultPage(request):
+    return redirect('posts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', defaultPage, name= 'defaultPage'),
     path('posts/', include('posts.urls'))
 ]
