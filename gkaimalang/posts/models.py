5bf807e9b2a796ae6a2757d65d6657019c230721
goenpiloes <goenpiloes@gmail.com>
2022-12-09 03:30:44 +0700

    intial query init.sql and post model, then register it to admin.py

--- gkaimalang/posts/models.py
+++ gkaimalang/posts/models.py
@@ -1,3 +1,8 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                            primary_key=True, editable=False)
+    title = models.CharField(max_length=200)
---
+++ init.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS posts_post;
+CREATE TABLE posts_post (
+    id
+    title varchar(200) NOT NULL,
+);
