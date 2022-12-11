9f58ec7621914e1c00c78bf8d1c409dbb8a24c69
goenpiloes <goenpiloes@gmail.com>
2022-12-11 23:49:15 +0700

    add configuration for table name in db

--- gkaimalang/posts/models.py
+++ gkaimalang/posts/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                             primary_key=True, editable=False)
     title = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'posts'
