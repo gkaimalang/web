a685be1e2d695ca367ba960dc69ac985698a9dd6
goenpiloes <goenpiloes@gmail.com>
2022-12-14 02:21:09 +0700

    add bootstrap

--- gkaimalang/posts/views.py
+++ gkaimalang/posts/views.py
@@ -4,5 +4,5 @@ def posts(request):
     return render(request, 'posts/posts.html')
 
 def post(request, id):
-    context = {id: id}
+    context = {'id': id}
     return render(request, 'posts/post.html', context)
